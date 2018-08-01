2357f4504a79d1655e9931a9a7cae3085becfbcd
Ryuuji Yoshimoto <ryuuji@calil.jp>
2018-08-01 13:20:42 +0900

    OpenURLの実装を追加

--- src/js/history.js
+++ src/js/history.js
@@ -22,14 +22,55 @@ function locationEnabled() {
  */
 function queryParse(text: string): UnitradQuery & { q?: string, filter?: string, mode?: string } {
   let tmp = {};
+  let unknown = {};
   for (let k of URLPARAMS) tmp[k] = '';
   text.split('&').forEach((kv) => {
     let _tmp = kv.split('=');
     let _k = _tmp[0];
     if (tmp.hasOwnProperty(_k)) {
       tmp[_k] = decodeURIComponent(_tmp[1]);
+    } else {
+      unknown[_k] = decodeURIComponent(_tmp[1]);
     }
   });
+  // OpenURLに関する処理
+  if (unknown.url_ver === 'Z39.88-2004' || unknown.url_ver === 'z39.88-2004') {
+    for (let k in unknown) {
+      if (unknown.hasOwnProperty(k)) {
+        let key = k.toLowerCase();
+        switch (key) {
+          case 'btitle':
+          case 'jtitle':
+          case 'rft.title':
+          case 'rft.btitle':
+          case 'rft.jtitle':
+            tmp.title += unknown[k];
+            break;
+          case 'au':
+          case 'aulast':
+          case 'aufirst':
+          case 'rft.au':
+          case 'rft.aulast':
+          case 'rft.aufirst':
+            tmp.author += unknown[k];
+            break;
+          case 'pub':
+          case 'rft.publisher':
+          case 'rft.pub':
+            tmp.publisher += unknown[k];
+            break;
+          case 'date':
+          case 'rft.date':
+            tmp.year_start = unknown[k];
+            tmp.year_end = unknown[k];
+            break;
+          case 'rft.isbn':
+            tmp.isbn = unknown[k];
+            break;
+        }
+      }
+    }
+  }
   return tmp;
 }
 
